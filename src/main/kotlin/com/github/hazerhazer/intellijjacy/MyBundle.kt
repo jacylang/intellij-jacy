5d4c0c7d833e8025d84da9f1bdf6cc263a1aa06f
GitHub Action <action@github.com>
2021-09-20 19:34:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/hazerhazer/intellijjacy/MyBundle.kt
+++ src/main/kotlin/com/github/hazerhazer/intellijjacy/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.hazerhazer.intellijjacy
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/hazerhazer/intellijjacy/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/hazerhazer/intellijjacy/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.hazerhazer.intellijjacy.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.hazerhazer.intellijjacy.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
